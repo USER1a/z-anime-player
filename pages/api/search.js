568c5cade951bffff323aefc30152c091fa93864
USER1a <anderszame@gmail.com>
2025-09-02 15:05:31 +0530

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,6 +4,15 @@ const ANIMEWORLD_API_URL = "https://animeworlda.vercel.app";
 const TANIME_API_URL = "https://tanime.tv/api";
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
